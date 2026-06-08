50115a7077fa03f5d1d143760c77eb4863c4e241
yerodigo-cell <yerodigo@gmail.com>
2026-06-07 21:25:35 -0500

    Fix PHP Fatal Error: restore encode_content_links visibility to public static to match Moodle core parent class

--- backup/moodle2/backup_codeframe_activity_task.class.php
+++ backup/moodle2/backup_codeframe_activity_task.class.php
@@ -53,7 +53,7 @@ class backup_codeframe_activity_task extends backup_activity_task {
      * @param string $content The content to encode.
      * @return string The encoded content.
      */
-    protected static function encode_content_links($content) {
+    public static function encode_content_links($content) {
         global $CFG;
 
         $base = preg_quote($CFG->wwwroot, '/');
